8e9b11b9f6ba4adf19ad0e5685a2ca066f84aebd
蒙大拿 <977466992@qq.com>
2021-04-29 15:30:01 +0800

    swagger 、 lombok

---
+++ mars-swagger/src/main/java/com/mqz/mars/swagger/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.mqz.mars.swagger;
+
+/**
+ * 青网科技集团 版权所有 © Copyright 2012<br>
+ *
+ * @Author： 蒙大拿
+ * @Date：2021/4/26 3:45 下午
+ * @Description
+ * @About： https://github.com/DemoMeng
+ */
+public class SwaggerConfig {
+}
