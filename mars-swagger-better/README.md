f90dd10905f341f0785eb74ce9b29a5401b82448
蒙大拿 <977466992@qq.com>
2021-09-03 08:40:17 +0800

    mars-swagger-better集成knife4j及swagger配置

---
+++ mars-swagger-better/README.md
@@ -0,0 +1,7 @@
+# the swagger api document power by mqz with knif4j
+
+ - swagger
+ - knife4j： knife4j-boot-starter为2.0.7
+ - springboot: 当前Springboot版本为2.3.5.RELEASE
+
+
--- mars-swagger-better/src/main/java/com/mqz/mars/swagger/better/config/SwaggerKnife4jConfig.java
+++ mars-swagger-better/src/main/java/com/mqz/mars/swagger/better/config/SwaggerKnife4jConfig.java
@@ -1,6 +1,5 @@
-package com.mqz.mars.swagger;
+package com.mqz.mars.swagger.better.config;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,25 +10,23 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
- *  版权所有 © Copyright 2012<br>
+ * 青网科技集团 版权所有 © Copyright 2012<br>
  *
  * @Author： 蒙大拿
- * @Date：2021/4/26 3:45 下午
+ * @Date：2021/9/2 10:32 上午
  * @Description
  * @About： https://github.com/DemoMeng
  */
 @Configuration
-@EnableSwagger2
-@EnableSwaggerBootstrapUI()//使用SwaggerBootstrapUI的登陆名及密码校验
-public class SwaggerConfig {
-
+@EnableSwagger2WebMvc
+public class SwaggerKnife4jConfig {
 
     @Value("${swagger.enabled:false}")
     private boolean enable;
-    @Value("${swagger.info.title}")
+    @Value("${swagger.info.title:接口}")
     private String title;
     @Value("${swagger.info.description}")
     private String description;
@@ -55,7 +52,6 @@ public class SwaggerConfig {
                 .build();
     }
 
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title(title)
@@ -66,13 +62,4 @@ public class SwaggerConfig {
                 .version(version)
                 .build();
     }
-
-
-
-
-
-
-
-
-
 }
